5b1bd2c5962acde3518a33ec4c6ef3a60f7f9101
KarpovskyiD <karpovskyi.d@gmail.com>
2020-12-11 18:39:41 +0200

    Initial commit

---
+++ Models/book.rb
@@ -0,0 +1,8 @@
+class Book
+  attr_accessor title, autor
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+end
---
+++ Models/order.rb
@@ -0,0 +1,9 @@
+class Order
+  attr_accessor book, reader, date
+
+  def initialize(title, author)
+    @book = book
+    @reader = reader
+    @date = date
+  end
+end
