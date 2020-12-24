8d1679fb47224ab597cbea716d80bd5847726ea0
KarpovskyiD <karpovskyi.d@gmail.com>
2020-12-24 18:37:25 +0200

    add_readers seed method is updated

--- seeds.rb
+++ seeds.rb
@@ -13,9 +13,10 @@ module Seeds
     books = ['Da Vinci Code', 'Origin', 'Inferno', "Harry Potter and the Philosopher's Stone",
              'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Prisoner of Azkaban',
              'Gelseminum as a poison', 'A Scandal in Bohemia', 'The Green Flag']
+    readers = %w[DK DM DS]
     add_authors(library, authors)
     add_books(library, books)
-    add_readers(library)
+    add_readers(library, readers)
     add_orders(library)
     library.write_to_yaml(file_name)
   end
@@ -34,13 +35,11 @@ module Seeds
     end
   end
 
-  def self.add_readers(library)
-    reader1 = Reader.new({ name: 'DK', email: 'dk@gmail.com', city: 'D', street: 'street', house: 1 })
-    reader2 = Reader.new({ name: 'DM', email: 'dk@gmail.com', city: 'D', street: 'street', house: 1 })
-    reader3 = Reader.new({ name: 'DS', email: 'dk@gmail.com', city: 'D', street: 'street', house: 1 })
-    library.readers << reader1
-    library.readers << reader2
-    library.readers << reader3
+  def self.add_readers(library, list)
+    list.each do |r|
+      reader = Reader.new({ name: r, email: 'dk@gmail.com', city: 'D', street: 'street', house: 1 })
+      library.readers << reader
+    end
   end
 
   def self.add_orders(library)
